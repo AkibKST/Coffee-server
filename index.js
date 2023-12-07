54f219e4f52d89d07278abc5743692b145a9f10d
AkibKST <akibkst22@gmail.com>
2023-12-07 13:14:14 +0600

    delete a item

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,12 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-// coffeeMaster
-// U9djvTnkn3Xy66oN
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://coffeeMaster:U9djvTnkn3Xy66oN@cluster0.s1acxwp.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1acxwp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +25,54 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // newCoffee ke data base e pathano
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+    // mongodb theke data cliennt side e dekhabo
+    app.get('/coffee', async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        // coffeeCollection er modde sob gula data client side e dekhabo
+
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // update korar jonno
+    app.get('/coffee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.findOne(query);
+        res.send(result)
+    })
+    // client side theke newCoffee ta nilam
+    app.post('/coffee', async(req, res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+
+        // client side theke j data ta asche seta cltn er moddhe dibo
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result)
+    })
+
+    // mongodb theke delete korar kaj
+    app.delete('/coffee/:id', async (req, res)=>{
+        // id ta res.params.id theke niye nibo
+        const id = req.params.id;
+        // khujbo jeta diye delete korbo
+        const query = { _id: new ObjectId(id)};
+        // mongodb crud er usecase theke ekta delete oparation chalabo
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
